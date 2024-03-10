e0ed9c537a7317098b2e258def55cbadc9910c2f
NguyenDat-2003 <maxdat2003@gmail.com>
2024-03-10 16:17:07 +0700

    Update_CORS_not_allow_localhost

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,7 @@
 // --- Những domain được phép truy cập đén tài nguyên server
-export const WHITELIST_DOMAINS = ['http://localhost:5173']
+export const WHITELIST_DOMAINS = [
+  // 'http://localhost:5173'
+]
 export const BOARD_TYPES = {
   PUBLIC: 'public',
   PRIVATE: 'private'
