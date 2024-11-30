30355321aad83314081bd79fe9680c27c3fc2ec4
Nguyen Tien Dat <maxdat2003@gmail.com>
2024-11-30 21:11:51 +0700

    add new whitelist cors domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,8 @@
 // --- Những domain được phép truy cập đén tài nguyên server
-export const WHITELIST_DOMAINS = ['http://localhost:5173']
+export const WHITELIST_DOMAINS = [
+  // 'http://localhost:5173',
+  'https://my-trello-web-eight.vercel.app'
+]
 export const BOARD_TYPES = {
   PUBLIC: 'public',
   PRIVATE: 'private'
